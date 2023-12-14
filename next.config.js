7a25eeccea8326018f592b858e6ef5050bad2e13
LxxsCLN <luisxxcanada@gmail.com>
2023-12-13 22:33:00 -0600

    Ignore eslint on build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { eslint: { ignoreDuringBuilds: true } }
 
 //module.exports = nextConfig
 
 const withNextIntl = require('next-intl/plugin')();
- 
+
 module.exports = withNextIntl(nextConfig);
